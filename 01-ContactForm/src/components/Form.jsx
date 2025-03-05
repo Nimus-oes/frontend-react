9ab2b97b1d65f7cc045f7eb71fc0193aabbd364c
Nimus-oes <sseo.code@gmail.com>
2025-03-05 16:50:53 +0900

    Add responsive layout to form
    A grid layout is applied to switch between mobile and tablet/desktop screens.

--- 01-ContactForm/src/components/Form.jsx
+++ 01-ContactForm/src/components/Form.jsx
@@ -1,39 +1,43 @@
 export default function Form() {
   return (
-    <form>
-      <label htmlFor="name">
+    <form className="form">
+      <label htmlFor="name" className="input-item name">
         Name
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" className="input-field" />
       </label>
-      <label htmlFor="email">
+      <label htmlFor="email" className="input-item email">
         Company Email
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" className="input-field" />
       </label>
-      <label htmlFor="company-size">
+      <label htmlFor="company-size" className="input-item size">
         Company Size
-        <select name="company-size" id="company-size">
+        <select name="company-size" id="company-size" className="input-field">
           <option value="sm">50-100 employees</option>
           <option value="md">100-200 employees</option>
           <option value="lg">Over 200 employees</option>
         </select>
       </label>
-      <label htmlFor="subject">
+      <label htmlFor="subject" className="input-item subject">
         Subject
-        <select name="subject" id="subject">
+        <select name="subject" id="subject" className="input-field">
           <option value="">Building Landing Pages</option>
           <option value="">About Pricing</option>
           <option value="">Other</option>
         </select>
       </label>
-      <label htmlFor="message">
+      <label htmlFor="message" className="input-item message">
         Message
-        <textarea name="message" id="message"></textarea>
+        <textarea
+          name="message"
+          id="message"
+          className="input-field"
+        ></textarea>
       </label>
-      <label htmlFor="consent">
+      <label htmlFor="consent" className="input-item consent">
         <input type="checkbox" name="consent" id="consent" /> I consent to being
         contacted by the team.
       </label>
-      <button>Contact Sales</button>
+      <button className="button">Contact Sales</button>
     </form>
   );
 }
