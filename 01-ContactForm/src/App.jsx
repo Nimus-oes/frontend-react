0e3478b231f6e3b8cf5204cafa4719dce33624c0
Nimus-oes <sseo.code@gmail.com>
2025-03-05 15:31:50 +0900

    Create component structure

--- 01-ContactForm/src/App.jsx
+++ 01-ContactForm/src/App.jsx
@@ -1,3 +1,18 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Title from "./components/Title";
+import Subtitle from "./components/Subtitle";
+import Form from "./components/Form";
+
 export default function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <Title />
+        <Subtitle />
+        <Form />
+      </Main>
+    </>
+  );
 }
